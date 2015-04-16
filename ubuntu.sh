ccbf355a161304226237b1179c1c729b4e3591f4
canino_jories <canino_jories@hotmail.com>
2015-04-16 17:36:06 +0800

    adding zsh

--- ubuntu.sh
+++ ubuntu.sh
@@ -16,8 +16,19 @@ sudo chsh -s $(which zsh) $(whoami)
 \curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 #get the stable version of rvm
 \curl -sSL https://get.rvm.io | bash -s stable
-sudo echo "export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting" >>  $HOME/.zshrc --auto-dotfiles
+sudo echo "export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting"
 #install rvm dependencies
 rvm requirements
+
+#before getting the linuxbrew
+#install first the dependecies
+#https://github.com/Homebrew/linuxbrew
+sudo apt-get install build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+#get the latest linuxbrew from ruby
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+#add the path for linuxbrew
+sudo echo "export PATH="$HOME/.linuxbrew/bin:$PATH""
+sudo echo "export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH""
+sudo echo "export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH""
 
 echo "========== FINISH!!! =========="
