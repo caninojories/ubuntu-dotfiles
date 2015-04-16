67b817a613871add7256dca0e77ab7182ab9d8dc
canino_jories <canino_jories@hotmail.com>
2015-04-16 17:11:02 +0800

    adding zsh

--- ubuntu.sh
+++ ubuntu.sh
@@ -16,8 +16,8 @@ sudo chsh -s $(which zsh) $(whoami)
 \curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 #get the stable version of rvm
 \curl -sSL https://get.rvm.io | bash -s stable
-source $HOME/.rvm/scripts/rvm
-sudo echo "export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting" >>  $HOME/.zshrc
+source $PATH:$HOME/.rvm/scripts/rvm
+sudo echo "export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting" >>  $PATH:$HOME/.zshrc
 #install rvm dependencies
 rvm requirements
 
