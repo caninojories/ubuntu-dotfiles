efd923db7e68b62523a28d836626657130aab4a3
canino_jories <canino_jories@hotmail.com>
2015-04-16 16:31:53 +0800

    adding rvm

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,12 +7,16 @@
 sudo apt-get update
 sudo apt-get install curl
 
+#for source to work download zsh
+sudo apt-get install zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+chsh -s /bin/zsh
+
 #before curling just download this first
 \curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 #get the stable version of rvm
 \curl -sSL https://get.rvm.io | bash -s stable
 source $HOME/.rvm/scripts/rvm
-exit
 sudo echo "export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting"
 #install rvm dependencies
 rvm requirements
