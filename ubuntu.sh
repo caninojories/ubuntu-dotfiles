20583a9736ee3380e1928628f741f49ddf2f8356
canino_jories <canino_jories@hotmail.com>
2015-04-16 23:11:28 +0800

    adding brew if else

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,8 +4,8 @@
 # Install curl
 # ============================================================================
 
-sudo apt-get update
-sudo apt-get install curl
+apt-get update
+apt-get install curl
 
 #for source to work download zsh
 sudo apt-get install zsh
@@ -16,7 +16,8 @@ sudo chsh -s $(which zsh) $(whoami)
 \curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 #get the stable version of rvm
 \curl -sSL https://get.rvm.io | bash -s stable && rvm reload
-sudo echo "export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting"
+source $HOME/.rvm/scripts/rvm
+sudo echo "export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting" >>  $HOME/.zshrc
 #install rvm dependencies
 rvm requirements
 
