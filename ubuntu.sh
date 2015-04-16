dd18ffee8dec37f1011de1b9b84cf8dff048339f
canino_jories <canino_jories@hotmail.com>
2015-04-16 16:09:31 +0800

    adding rvm

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,9 +5,14 @@
 # ============================================================================
 
 sudo apt-get update
-sudo apt-get curl
+sudo apt-get install curl
 
 #get the stable version of rvm
 \curl -L https://get.rvm.io | bash -s stable
+source $HOME/.rvm/scripts/rvm
+sudo echo "export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting"
 #install rvm dependencies
 rvm requirements
+
+
+echo "========== FINISH!!! =========="
