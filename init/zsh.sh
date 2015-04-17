f3138b4eeb7599132e44ed591fc6396deaffb045
canino_jories <canino_jories@hotmail.com>
2015-04-17 21:57:27 +0800

    adding .bash

--- init/zsh.sh
+++ init/zsh.sh
@@ -13,6 +13,8 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Installing oh-my-zsh"
   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 fi
+sudo apt-get install zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 sudo chsh -s $(which zsh) $(whoami)
 
 # Open Terminal Settings
