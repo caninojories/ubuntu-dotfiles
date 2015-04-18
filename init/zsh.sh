a764c03b83206414652e05ba551cbac26da2e973
canino_jories <canino_jories@hotmail.com>
2015-04-18 21:45:57 +0800

    sourcing zshrc

--- init/zsh.sh
+++ init/zsh.sh
@@ -5,11 +5,13 @@ DOT="$HOME/GITENVREPO/env"
 echo "$(tput setaf 6)INITIALIZING .bash_aliases"
 echo "$(tput setaf 5)"
 cp -Rfxv $DOT/shell/.bash_aliases $HOME/.bash_aliases
+echo ""
 echo "$(tput setaf 1)========== FINISH =========="
 
 echo "$(tput setaf 6)INITIALIZING .bash_profile"
 echo "$(tput setaf 5)"
 cp -Rfxv $DOT/shell/.bash_profile $HOME/.bash_profile
+echo ""
 echo "$(tput setaf 1)========== FINISH =========="
 
 # Install zsh & oh-my-zsh
@@ -19,6 +21,7 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sudo apt-get install zsh
   curl -L http://install.ohmyz.sh | sh
   echo "$(tput setaf 1)========== FINISH =========="
+  echo ""
 fi
 
 # Link .zshrc
@@ -28,6 +31,7 @@ echo "$(tput setaf 5)"
 cp -Rfxv "$DOT/shell/jories.zsh-theme" "$HOME/.oh-my-zsh/themes/jories.zsh-theme"
 cp -Rfxv "$DOT/shell/.zshrc" "$HOME/.zshrc"
 echo "$(tput setaf 1)========== FINISH ========="
+echo ""
 #Use the zsh in our current shell
 echo "$(tput setaf 6)MAKE THE ZSH OUR DEFAULT SHELL"
 echo "$(tput setaf 5)"
@@ -35,5 +39,6 @@ sudo chsh -s $(which zsh) $(whoami)
 echo "$(tput setaf 1)========== FINISH =========="
 
 sleep 1
+echo ""
 echo ""
 echo "$(tput setaf 2)========== END!!! =========="
