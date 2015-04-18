2468f3868932d786ed8c6e2a8048330ebde9aaf9
canino_jories <canino_jories@hotmail.com>
2015-04-18 20:18:04 +0800

    sourcing zshrc

--- init/zsh.sh
+++ init/zsh.sh
@@ -34,6 +34,6 @@ echo "$(tput setaf 5)"
 sudo chsh -s $(which zsh) $(whoami)
 echo "$(tput setaf 1)========== FINISH =========="
 
-cp -Rfxv "$DOT/shell/.zshrc" "$HOME/.zshrc" && . ~/.zshrc
 sleep 1
+echo ""
 echo "$(tput setaf 2)========== END!!! =========="
