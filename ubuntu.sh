6fd784ab07e9ccef5e00129d05a2e7fa9c6d9536
canino_jories <canino_jories@hotmail.com>
2015-04-20 15:44:34 +0800

    adding sleep to read by the user what happen in the console

--- ubuntu.sh
+++ ubuntu.sh
@@ -9,20 +9,24 @@
 if ! whereis git; then
   echo "$(tput setaf 6)DOWNLOADING GIT"
   echo "$(tput setaf 5)"
+  sleep 5
   sudo add-apt-repository ppa:git-core/ppa
   sudo apt-get update
   sudo apt-get install git
+  sleep 3
   echo " "
   echo "$(tput setaf 1)========== FINISH =========="
   echo " "
 else
   echo "$(tput setaf 6)AWESOME! GIT is INSTALLED! Now Updating Packages..."
-  echo ""
+  echo "$(tput setaf 5)"
+  sleep 5
   sudo apt-get dist-upgrade
   sudo apt-get update
   sudo apt-get autoremove
   sudo apt-get autoclean
   sudo apt-get clean
+  sleep 3
   echo " "
   echo "$(tput setaf 1)========== FINISH =========="
   echo " "
@@ -31,7 +35,9 @@ fi
 #install curl before running condition
 echo "$(tput setaf 6)DOWNLOADING CURL"
 echo "$(tput setaf 5)"
+sleep 5
 sudo apt-get install curl
+sleep 3
 echo " "
 echo "$(tput setaf 1)========== FINISH =========="
 echo " "
@@ -39,54 +45,66 @@ echo " "
 if ! whereis brew; then
   echo "$(tput setaf 6)DOWNLOADING And INSTALLING HOMEBREW"
   echo "$(tput setaf 5)"
+  sleep 5
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+  sleep 3
   echo " "
   echo "$(tput setaf 1)========== FINISH =========="
   echo " "
   # Run Brew doctor before anything else
   echo "$(tput setaf 6)RUNNING BREW DOCTOR"
   echo "$(tput setaf 5)"
+  sleep 5
   brew doctor
+  sleep 3
   echo " "
   echo "$(tput setaf 1)========== FINISH =========="
   echo " "
 else
   echo "$(tput setaf 6)AWESOME! HOMEBREW is INSTALLED! Now Updating..."
   echo "$(tput setaf 5)"
+  sleep 5
   brew upgrade
   brew update
+  sleep 3
   echo " "
   echo "$(tput setaf 1)========== FINISH =========="
   echo " "
 fi
 echo "$(tput setaf 6)COPY .BASHRC And .BASH_PROFILE"
 echo "$(tput setaf 5)"
+sleep 5
 cp -R $HOME/GITENVREPO/env/shell/.bashrc $HOME/.bashrc
 cp -R $HOME/GITENVREPO/env/shell/.bash_profile $HOME/.bash_profile
+sleep 3
 echo " "
 echo "$(tput setaf 1)========== FINISH =========="
 echo " "
 
 echo "$(tput setaf 6)CURL RVM"
 echo "$(tput setaf 5)"
+sleep 5
 #before curling just download this first
 \curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 #get the stable version of rvm
 \curl -sSL https://get.rvm.io | bash
+sleep 3
 echo " "
 echo "$(tput setaf 1)========== FINISH =========="
 echo " "
 
 echo "$(tput setaf 6)UPDATE REMOVE AUTOCLEAN CLEAN PACKAGES"
 echo "$(tput setaf 5)"
+sleep 5
 sudo apt-get update
 sudo apt-get autoremove
 sudo apt-get autoclean
 sudo apt-get clean
+sleep 3
 echo " "
 echo "$(tput setaf 1)========== FINISH =========="
 
-sleep 1
+sleep 3
 echo " "
 echo " "
 echo "$(tput setaf 2)========== END!!! =========="
