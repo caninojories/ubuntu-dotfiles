6378074bce90eda3ad837d8650fabf3940aabae8
Jo-Ries Canino <canino_jories@hotmail.com>
2015-05-05 12:18:28 +0800

    adding zsh

--- 1_shell.sh
+++ 1_shell.sh
@@ -33,7 +33,7 @@ echo ""
 echo "$(tput setaf 6) ⇣ Jories zsh-theme"
 echo "$(tput setaf 5)"
 cp -Rfxv "$DOT/bin/Jories Base-16.zsh-theme" "$HOME/.oh-my-zsh/themes/jories.zsh-theme"
-cp -Rfxv "$DOT/bin/.zshrc" "$HOME/.zshrc"
+cp -Rfxv "$DOT/bin/.zshrc" "$HOME/.zshrc" && . $HOME/.zshrc
 
 sudo chsh -s $(which zsh)
 #make the zsh our default
